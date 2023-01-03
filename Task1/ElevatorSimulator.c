f22ff94249887178ba4ec682686303a5a73e19d4
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-03 19:37:41 +0100

    +Asking user where to move to

--- Task1/ElevatorSimulator.c
+++ Task1/ElevatorSimulator.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 void PrintBuilding();
+void MoveElevetor();
+int CheckArrayFor();
 
 int main(){
     char floors[6] = {'G', '1', '2', '3', '4', '5'};
     char* pElevatorPosition = &floors[4];
+    char userDesiredFloor;
     PrintBuilding(pElevatorPosition, floors, 6);
     printf("\n  [] Elevator current floor: %c", *pElevatorPosition);
+    printf("\n  [?] Which floor would you like to go to? -Floor ");
+    //Requests user input till he enters a floor that exists
+    do{
+        scanf("%c", userDesiredFloor);
+    } while (!DesiredFloorExists(userDesiredFloor, floors, 6));
+
 }
 
 void PrintBuilding(char* pElevatorPosition, char floors[], int floorCount){
@@ -20,6 +29,19 @@ void PrintBuilding(char* pElevatorPosition, char floors[], int floorCount){
         }
     }
         printf("+-------+");
+}
+
+void MoveElevator(char floorToMoveTo){
+
+}
+
+int DesiredFloorExists(char floorToFind, char floors[], int floorCount){
+    for (int floor = 0; floor < floorCount; floor++){
+        if (floorToFind == floors[floor]){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 
