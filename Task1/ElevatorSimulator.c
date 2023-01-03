9a98f9e4f5aa7b54c1e51fc2700f129d989129a7
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-03 19:08:23 +0100

    +module  prints the building with correct lift pos

--- Task1/ElevatorSimulator.c
+++ Task1/ElevatorSimulator.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+void PrintBuilding();
 
 int main(){
-    
+    char floors[6] = {'G', '1', '2', '3', '4', '5'};
+    char* pElevatorPosition = &floors[4];
+    PrintBuilding(pElevatorPosition, floors, 6);
+    printf("\n  [] Elevator current floor: %c", *pElevatorPosition);
 }
+
+void PrintBuilding(char* pElevatorPosition, char floors[], int floorCount){
+        printf("+-------+\n");
+    for (int i = floorCount-1; i > -1; i--){
+        printf("|-     -|\n");
+        //Finds and prints the elevator on the correct floor
+        if (*pElevatorPosition == floors[i]){
+        printf("+- [ ] -+\n");
+        }else{
+        printf("+-  %c  -+\n", floors[i]);
+        }
+    }
+        printf("+-------+");
+}
+
+
