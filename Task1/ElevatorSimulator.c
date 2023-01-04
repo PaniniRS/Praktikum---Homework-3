5089403fb28363b4e0424438da8613974041d4b8
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-04 14:19:15 +0100

    Changes to findindex func

--- Task1/ElevatorSimulator.c
+++ Task1/ElevatorSimulator.c
@@ -2,16 +2,28 @@
 #include <stdbool.h>
 void PrintBuilding();
 void MoveElevetor();
-bool DesiredFloorExists();
+void FindIndexDesiredFloor();
 
 int main(){
     char floors[6] = {'G', '1', '2', '3', '4', '5'};
-    char* pElevatorPosition = &floors[4];
+    char* pElevatorPosition = &floors[5];
     char userDesiredFloor;
+    bool userDesiredFloorExists = false;
     PrintBuilding(pElevatorPosition, floors, 6);
     printf("\n  [] Elevator current floor: %c", *pElevatorPosition);
-    printf("\n  [?] Which floor would you like to go to? -Floor ");
     //Requests user input till he enters a floor that exists
+    // do{
+        printf("\n  [?] Which floor would you like to go to? -Floor ");
+        scanf("%c", &userDesiredFloor);
+        FindIndexDesiredFloor(userDesiredFloor, floors, 6);
+        //checks whether the inputed floor exits 
+    //     for (int i = 0; i < 6; i++){
+    //         if (userDesiredFloor == floors[i]){
+    //             userDesiredFloorExists = true;
+    //         }
+    //     }
+        
+    // } while (!userDesiredFloorExists);
     
     
     
@@ -33,16 +45,13 @@ void PrintBuilding(char* pElevatorPosition, char floors[], int floorCount){
 }
 
 void MoveElevator(char floorToMoveTo){
-
+    
 }
 
-bool DesiredFloorExists(char floorToFind, char floors[6], int floorCount){
-    for (int floor = 0; floor < floorCount; floor++){
-        if (floorToFind == floors[floor]){
-            return true;
+void FindIndexDesiredFloor(char* desiredFloor, char floors[], int floorCount){
+    for (int i = 0; i < floorCount; i++){
+        if (desiredFloor == floors[i]){
+            printf("%i", i);
         }
     }
-    return false;
 }
-
-
