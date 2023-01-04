b968f9a8f7a611d20ce309bab7cc2370b453465f
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-04 20:56:47 +0100

    Added basement

--- Task1/ElevatorSimulator.c
+++ Task1/ElevatorSimulator.c
@@ -9,13 +9,14 @@ void MoveElevator();
 int FindIndex();
 
 int main(){
-    char floors[6] = {'G', '1', '2', '3', '4', '5'};
+    int arrayLength = 9;
+    char floors[9] = {'-3', '-2', '-1', 'G', '1', '2', '3', '4', '5'};
     char* pElevatorPosition = &floors[0];
     char userDesiredFloor;
     bool userDesiredFloorExists = false;
     // bool userWantsToExit = false;
 
-    PrintBuilding(pElevatorPosition, floors, 6);
+    PrintBuilding(pElevatorPosition, floors, arrayLength);
     printf("\n\t[] Elevator current floor: %c", *pElevatorPosition);
 
     printf("\n\t[?] Which floor would you like to go to? -Floor ");
@@ -23,19 +24,19 @@ int main(){
     //first time the elevator is moved
     scanf("%c", &userDesiredFloor);
     char* pUserDesiredFloor = &userDesiredFloor;
-    MoveElevator(pElevatorPosition, pUserDesiredFloor, floors, 6);
+    MoveElevator(pElevatorPosition, pUserDesiredFloor, floors, arrayLength);
 
     //TODO: proveri dali postoi spratot
     while (true){
-        printf("\n\t Write E to exit or which floor you want the lift to move to: ");
-        scanf("%c", &userDesiredFloor);
+        char userDesiredFloor;
+        printf("\n\t [?] Write E to exit or which floor you want the lift to move to: ");
+        scanf("%c", &userDesiredFloor); // ne prima vrednost
         char* pUserDesiredFloor = &userDesiredFloor;
+
         if(userDesiredFloor == 'E'){
             break;
         }
-        else{
-            MoveElevator(pElevatorPosition, pUserDesiredFloor, floors, 6);
-        }
+        MoveElevator(pElevatorPosition, pUserDesiredFloor, floors, arrayLength);
     }
 }
 
@@ -44,7 +45,7 @@ int main(){
 //=----------------------------------------------------------------------------=
 
 void PrintBuilding(char* pElevatorPosition, char floors[], int floorCount){
-        printf("+-------+\n");
+        printf("\n+-------+\n");
     for (int i = floorCount-1; i > -1; i--){
         printf("|-     -|\n");
         //Finds and prints the elevator on the correct floor
@@ -71,10 +72,10 @@ void MoveElevator(char* pElevatorPosition, char* pFloorToMoveTo, char array[], i
             pElevatorPosition = &array[elevatorIndex-1];
         }
     }
-            PrintBuilding(pElevatorPosition, array, 6);
+            PrintBuilding(pElevatorPosition, array, arrayLength);
 }
 
-int FindIndex(char* elementToFind, char array[6], int arrayLength){
+int FindIndex(char* elementToFind, char array[9], int arrayLength){
     int elementIndex;
     for (int i = 0; i < arrayLength; i++){
         char elementOfArray = array[i];
