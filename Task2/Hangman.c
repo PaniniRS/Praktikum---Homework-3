b6146bbdb2267fce027980b59f17be88d10863b5
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-05 18:45:29 +0100

    +Made functions and added libs

--- Task2/Hangman.c
+++ Task2/Hangman.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char PickRandomWord();
+void Game();
+void PrintGameOutput();
+void CheckGuess();
+char GetUserGuess();
 
 int main(){
     
+    int wrongGuesses = 0;
+    char* words[] ={"Test","Dog" , "Cat"};
+
+
+}
+
+char PickRandomWord(char words[], int arrayLength){
+    char word;
+    int wordIndex = (rand() % (arrayLength - 0 + 1)) + 0;
+    
+    return word;
+}
+void Game(){
+
+}
+void PrintGameOutput(){
+
+}
+void CheckGuess(){
+
+}
+char GetUserGuess(){
+
 }
