4ae0c2773fa11bff96fe8a7ecf7849ce92af69eb
PaniniRS <79162556+PaniniRS@users.noreply.github.com>
2023-01-05 19:24:23 +0100

    updated

--- Task2/Hangman.c
+++ Task2/Hangman.c
@@ -2,40 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-char PickRandomWord();
 void PrintGameOutput();
 void PrintStickman();
 void PrintGuessingWord();
 void CheckGuess();
 char GetUserGuess();
+#define ARR_SIZE(array) ( sizeof((array)) / sizeof((array[0])) )
+
 
 int main(){
     int wrongGuesses = 0;
+    char userGuess;
     char* words[] ={"Test", "Dog", "Cat"};
-    char randomWord = PickRandomWord(words, 3); 
-    printf("%s", randomWord);
+    char* randomWord = words[rand() % ARR_SIZE(words)];
+    printf("Random word selected: %s", randomWord);
+
+    //
+    printf("\nEnter your guess character: ");
+    scanf(" %s", &userGuess);
+    //
+    // char userGuess = GetUserGuess();
+    // char* pUserGuess = &userGuess;
+
+    printf("UserGuess: %c", userGuess);
+
 }
-
-char PickRandomWord(char words[], int arrayLength){
-    int wordIndexRandom = (rand() % (arrayLength - 0 + 1)) + 0;
-    char word = words[wordIndexRandom];
-
-    return word;
-}
-
 
 void PrintGameOutput(){
 
 }
-void CheckGuess(char* pUserGuess, char array[], char guessedArray[], int arrayLength){
-    for (int i = 0; i < arrayLength; i++){
-        if (array[i] == pUserGuess){
-            guessedArray[i] = array[i];
-        }
+// void CheckGuess(char* pUserGuess, char array[], char guessedArray[], int arrayLength){
+//     for (int i = 0; i < arrayLength; i++){
+//         if (pUserGuess == array[i]){
+//             guessedArray[i] = array[i];
+//         }
         
-    }
+//     }
     
-}
+// }
 void PrintStickman(){
 
 }
@@ -43,7 +47,7 @@ void PrintGuessingWord(){
 
 }
 char GetUserGuess(){
-    char userGuess = "";
+    char userGuess;
     printf("\nEnter your guess character: ");
     scanf(" %s", userGuess);
     return userGuess;
